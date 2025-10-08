f51c9db3477e3319e95285f9bfc4a9f43b05fc56
Fritz Grabo <hello@fritzgrabo.com>
2025-10-08 14:43:24 +0200

    Open external links in a new window/tab

--- source/static/scripts/main.js
+++ source/static/scripts/main.js
@@ -1,1 +1,16 @@
-// TBD
+// Open external links in a new window/tab.
+
+(function() {
+  try {
+    document.addEventListener('DOMContentLoaded', function() {
+      document.querySelectorAll('a[href]').forEach(link => {
+        if (/^https?:\/\//i.test(link.getAttribute('href'))) {
+          console.log(link.href)
+          link.target = '_blank';
+          link.rel = 'noopener noreferrer';
+        }
+      });
+    });
+  } catch (e) {
+  }
+})();
